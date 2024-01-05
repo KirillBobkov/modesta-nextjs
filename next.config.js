095b9ecf478a1736af819648497072c9ba5c4fb4
Kirill Bobkov <kirill_2105@mail.ru>
2024-01-05 17:07:28 +0400

    deploy github

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-module.exports = {
-    reactStrictMode: true,
-}
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+    images: { unoptimized: true },
+   
+    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+    // trailingSlash: true,
+   
+    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+    // skipTrailingSlashRedirect: true,
+   
+    distDir: 'docs',
+  }
+   
+  module.exports = nextConfig
