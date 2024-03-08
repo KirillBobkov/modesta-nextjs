d279e449ac578512d8c130e142f1b49847bbc92f
Kirill Bobkov <kirill_2105@mail.ru>
2024-03-08 18:36:27 +0400

    fixed tech block

--- components/TechBlock/TechBlock.jsx
+++ components/TechBlock/TechBlock.jsx
@@ -6,10 +6,42 @@ import tech_background_white from "../../public/images/tech_background_white.web
 import tech_background_black from "../../public/images/tech_background_black.webp";
 import tech_background_white_mobile from "../../public/images/tech_background_white_mobile.webp";
 import tech_background_black_mobile from "../../public/images/tech_background_black_mobile.webp";
+import tech_block_img_1 from "../../public/images/tech_block_img_1.jpg";
+import tech_block_img_2 from "../../public/images/tech_block_img_2.jpg";
+import tech_block_img_3 from "../../public/images/tech_block_img_3.jpg";
+import tech_block_img_4 from "../../public/images/tech_block_img_4.jpg";
 
 import styles from "./TechBlock.module.css";
 import { useMediaQuery } from "../../hooks/useMediaQuery.jsx";
 import containerStyles from "../../styles/container.module.css";
+import shadowStyles from "../../styles/shadow.module.css";
+
+const items = [
+  {
+    img: tech_block_img_1,
+    text: "Вес кисти",
+    key: '300',
+    key_param: 'грамм'
+  },
+  {
+    img: tech_block_img_2,
+    text: "Сила хвата",
+    key: '25',
+    key_param: 'кг'
+  },
+  {
+    img: tech_block_img_3,
+    text: "Угол вращения кисти",
+    key: '180°',
+    key_param: ''
+  },
+  {
+    img: tech_block_img_4,
+    text: "Ширина хвата",
+    key: '115',
+    key_param: 'мм'
+  },
+]
 
 export function TechBlock() {
   const { theme } = useTheme();
@@ -38,39 +70,16 @@ export function TechBlock() {
         Технические характеристики
       </h2>
       <ul className={styles.tech}>
-        <VisibilityManager as="li" className={`${styles.tech__item} shadow`}>
+        {items.map((item, i) => {
+          return  <VisibilityManager key={i} style={{ backgroundImage: `url(${item.img.src})` }} as="li" className={`${styles.tech__item} ${shadowStyles.shadow}`}>
           <div className={styles.tech__content}>
-            <span className={styles.tech__text}>
-              Максимальная ширина раскрытия хвата <br />
-              <span className={styles["tech__key-feature"]}>115</span> мм
+          <span className={styles.tech__text}>
+              {item.text} <br />
+              <span className={styles["tech__key-feature"]}>{item.key}</span>{item.key_param}
             </span>
           </div>
         </VisibilityManager>
-        <VisibilityManager as="li" className={`${styles.tech__item} shadow`}>
-          <div className={styles.tech__content}>
-            <span className={styles.tech__text}>
-              Вес кисти — не более <br />
-              <span className={styles["tech__key-feature"]}>109</span> грамм
-            </span>
-          </div>
-        </VisibilityManager>
-        <VisibilityManager as="li" className={`${styles.tech__item} shadow`}>
-          <div className={styles.tech__content}>
-            <span className={styles.tech__text}>
-              Тяговый протез выдержит силу хвата свыше <br />
-              <span className={styles["tech__key-feature"]}>25</span> кг
-            </span>
-          </div>
-        </VisibilityManager>
-        <VisibilityManager as="li" className={`${styles.tech__item} shadow`}>
-          <div className={styles.tech__content}>
-            <span className={styles.tech__text}>
-              Угол ротации (вращения) устройства
-              <br />
-              <span className={styles["tech__key-feature"]}>30°</span>
-            </span>
-          </div>
-        </VisibilityManager>
+        })}
       </ul>
     </VisibilityManager>
   );
