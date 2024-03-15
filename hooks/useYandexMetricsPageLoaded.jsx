cb4825867c6da8e2c5e979a7ef9664d27a6b7de1
Kirill Bobkov <kirill_2105@mail.ru>
2024-03-15 19:34:05 +0400

    fix metrika 3

--- hooks/useYandexMetricsPageLoaded.jsx
+++ hooks/useYandexMetricsPageLoaded.jsx
@@ -1,9 +1,7 @@
-import React, { useEffect } from "react";
-
 export function useYandexRobot(location) {
-    useEffect(() => {
-        ym(96028442, 'hit', location, {params:{
-            title: location,
-        }});
-    }, []);
+  ym(96028442, "hit", location, {
+    params: {
+      title: location,
+    },
+  });
 }
