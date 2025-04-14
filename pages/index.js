cf0c7203addc95f0d9e3f4928845bc01cd24c5f1
Kirill Bobkov <kirill_2105@mail.ru>
2025-04-15 00:15:14 +0300

    fix video compressed

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ import { useRouter } from "next/router.js";
 import { useYandexRobot } from "../hooks/useYandexMetricsPageLoaded.jsx";
 import { useEffect, useState } from "react";
 import { ProductsGrid } from "../components/Products/Products.jsx";
-import rotate from "../assets/images/rotate_new.mp4";
+import main_preview from "../assets/images/main_preview.mp4";
 
 const mainPageVideo = {
-  link: rotate,
+  link: main_preview,
   thumbnailUrl: rotate_poster.src,
   name: "Превью протеза руки человека",
   uploadDate: new Date("February 12, 2024").toISOString(),
