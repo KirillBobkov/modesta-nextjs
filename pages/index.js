2a30a0a98448d2713b203ba6487b9cbba508edad
Kirill Bobkov <kirill_2105@mail.ru>
2025-04-14 23:23:47 +0300

    fix 2

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,10 @@ import { useRouter } from "next/router.js";
 import { useYandexRobot } from "../hooks/useYandexMetricsPageLoaded.jsx";
 import { useEffect, useState } from "react";
 import { ProductsGrid } from "../components/Products/Products.jsx";
-import rotate from "../assets/images/rotate.mp4";
+import rotate22 from "../assets/images/rotate22.mp4";
 
 const mainPageVideo = {
-  link: rotate,
+  link: rotate22,
   thumbnailUrl: rotate_poster.src,
   name: "Превью протеза руки человека",
   uploadDate: new Date("February 12, 2024").toISOString(),
